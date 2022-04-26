675e3908fe3edc588099243517b5fda01e51ca74
Thompson Esther Chizoba <76252796+Apreal-T@users.noreply.github.com>
2022-04-26 13:41:56 +0000

    Update demo.spec.js

--- cypress/integration/demo.spec.js
+++ cypress/integration/demo.spec.js
@@ -2,37 +2,37 @@
 
 
 
-describe('assessment', () => {
-  before('test to find out I am on the right website', () => {
-      it('test to find out I am on the right website', ()=>{
-        cy.visit('https://www.mytruq.com')
+// describe('assessment', () => {
+//   before('test to find out I am on the right website', () => {
+//       it('test to find out I am on the right website', ()=>{
+//         cy.visit('https://www.mytruq.com')
      
-        cy.get('head > title').should('contain', 'truQ | The Smartest Way To Move Anything')
-        cy.get('.flex-wrap > :nth-child(1) > .items-center > .w-full > :nth-child(1)').should('have.text', 'What if moving')
-      })
+//         cy.get('head > title').should('contain', 'truQ | The Smartest Way To Move Anything')
+//         cy.get('.flex-wrap > :nth-child(1) > .items-center > .w-full > :nth-child(1)').should('have.text', 'What if moving')
+//       })
      
-    })
-    it('test to find out that website is responsive', () => {
-      cy.visit('https://www.mytruq.com')
-      cy.get('.style_lg_active').click().should('have.class', 'style_lg_active')
-      cy.get('.ml-10 > [href="/about"]').click()
-      cy.get('[href="/driver-partner"]').click()
-      cy.get('.ml-10 > [href="/business"]').click()
-    })
-    it('test to successful sign up', ()=>{
-      cy.visit('https://www.mytruq.com')
-      cy.get('.hidden > .flex > .button_primary').click()
-      cy.get('.swipe-button').click()
-      cy.get('.swipe-button').click()
-      cy.get('#root > div > div > div.fixed.w-full.z-20 > nav > div > div > div:nth-child(3) > div > div > button.button_primary.gap-x-2.lg\:gap-x-4.text-labels.lg\:text-sm-regular.xl\:px-8.px-3.py-3.undefined.undefined').click()
+//     })
+//     it('test to find out that website is responsive', () => {
+//       cy.visit('https://www.mytruq.com')
+//       cy.get('.style_lg_active').click().should('have.class', 'style_lg_active')
+//       cy.get('.ml-10 > [href="/about"]').click()
+//       cy.get('[href="/driver-partner"]').click()
+//       cy.get('.ml-10 > [href="/business"]').click()
+//     })
+//     it('test to successful sign up', ()=>{
+//       cy.visit('https://www.mytruq.com')
+//       cy.get('.hidden > .flex > .button_primary').click()
+//       cy.get('.swipe-button').click()
+//       cy.get('.swipe-button').click()
+//       cy.get('#root > div > div > div.fixed.w-full.z-20 > nav > div > div > div:nth-child(3) > div > div > button.button_primary.gap-x-2.lg\:gap-x-4.text-labels.lg\:text-sm-regular.xl\:px-8.px-3.py-3.undefined.undefined').click()
 
-    })
-    it('test to verify that I can login without sign up',()=>{
-      cy.visit('https://www.mytruq.com')
-      cy.get('.flex > .button_light').click()
-      cy.get('.swipe-button').click()
-      cy.get('.swipe-button').click()
-      cy.contains('<button class="button_light gap-x-2 lg:gap-x-4 text-labels lg:text-sm-regular xl:px-8 px-3 py-3 btn_bordered undefined" type="button">Log in</button>').click()
+//     })
+//     it('test to verify that I can login without sign up',()=>{
+//       cy.visit('https://www.mytruq.com')
+//       cy.get('.flex > .button_light').click()
+//       cy.get('.swipe-button').click()
+//       cy.get('.swipe-button').click()
+//       cy.contains('<button class="button_light gap-x-2 lg:gap-x-4 text-labels lg:text-sm-regular xl:px-8 px-3 py-3 btn_bordered undefined" type="button">Log in</button>').click()
       
-    })
-})
+//     })
+// })
